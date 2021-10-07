f4cc863a0b38eb95e26b0dd5691886d757343a2c
Groovyk08 <85116596+Groovyk08@users.noreply.github.com>
2021-10-07 16:23:46 +0200

    Add files via upload

--- modal.js
+++ modal.js
@@ -20,19 +20,21 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-
-// Fonction close pour fermer le formulaire
+// Fermeture du formulaire 
 function closeForm() {
   modalbg.style.display = "none";
 }
+
+// Confirmation du booking 
+const booking = document.querySelectorAll("booking-confirmed");
 
 // ID des éléments du formulaire 
 document.getElementById("first"); addEventListener("input", checkFirstName)
 document.getElementById("last"); addEventListener("input", checkLastName)
 document.getElementById("email"); addEventListener("input", checkEmail)
-document.getElementById("birhdate"); addEventListener("select", checkBirhdate)
+document.getElementById("birhdate"); addEventListener("click", checkBirhdate)
 document.getElementById("quantity"); addEventListener("click select", checkQuantity)
-document.getElementById("location1", "location2", "location3", "location4", "location5", "location6"); addEventListener("select", checkCity)
+document.getElementsByClassName("location"); addEventListener("change", checkCity)
 document.getElementById("checkbox1"); addEventListener("click", checkBox1)
 
 
@@ -82,12 +84,17 @@ let isEmailCorrect = false;
 
 function checkEmail() {
   let email = document.getElementById("email")
+
+  // REGEX pour s'assurer la bonne saisi de l'adresse mail
+  const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   // Si l'adresse mail est correcte
   if (email.value != null && email.value.length > 1) {
     email.parentNode.removeAttribute("data-error")
     email.parentNode.removeAttribute("data-error-visible")
     isEmailCorrect = true
   }
+
   // Si l'adresse mail est incorrecte
   else {
     email.parentNode.setAttribute("data-error", "Vous devez entrer une adresse mail valide.")
@@ -102,12 +109,14 @@ let isBirthdateCorrect = false;
 
 function checkBirhdate() {
   let birhdate = document.getElementById("birthdate")
+
   // Si la date de naissance est correcte
   if (birhdate.value != null && birhdate.value.length > 1) {
     birhdate.parentNode.removeAttribute("data-error")
     birhdate.parentNode.removeAttribute("data-error-visible")
     isBirthdateCorrect = true
   }
+
   // Si la date de naissance est incorrecte
   else {
     birhdate.parentNode.setAttribute("data-error", "Vous devez entrer votre date de naissance.")
@@ -122,12 +131,14 @@ let isQuantityCorrect = false;
 
 function checkQuantity() {
   let quantity = document.getElementById("quantity")
+
   // Si la valeur est correcte
-  if (quantity.value != null && quantity.value.length < 1) {
+  if (quantity.value != null) {
     quantity.parentNode.removeAttribute("data-error")
     quantity.parentNode.removeAttribute("data-error-visible")
     isQuantityCorrect = true
   }
+
   // Si la valeur est incorrecte
   else {
     quantity.parentNode.setAttribute("data-error", "Vous devez indiquer un nombre numérique comprise en 0 et 99.")
@@ -142,12 +153,14 @@ let isCityCorrect = false;
 
 function checkCity() {
   let city = document.querySelectorAll('input[name="location"]')
+
   // Si une ville est sélectionnée
-  if (city.value != null) {
+  if (city.checked == true) {
     city.parentNode.removeAttribute("data-error")
     city.parentNode.removeAttribute("data-error-visible")
     isCityCorrect = true
   }
+
   // Si une ville n'est pas sélectionnée
   else {
     city.parentNode.setAttribute("data-error", "Vous devez choisir une ville.")
@@ -162,12 +175,14 @@ let isCheckbox1Correct = false;
 
 function checkBox1() {
   let checkBox1 = document.getElementById("checkbox1")
+
   // Si une ville est sélectionnée
-  if (checkBox1.value.checked == true) {
+  if (checkBox1.checked == true) {
     checkBox1.parentNode.removeAttribute("data-error")
     checkBox1.parentNode.removeAttribute("data-error-visible")
     isCheckbox1Correct = true
   }
+
   // Si une ville n'est pas sélectionnée
   else {
     checkBox1.parentNode.setAttribute("data-error", "Vous devez accepter les conditions d'utilisation.")
@@ -180,11 +195,16 @@ function checkBox1() {
 // Soumettre le formulaire
 
 function validate() {
-  let submit = document.forms["reserve"]["first", "last", "email", "birthdate", "quantity", "location", "checkbox1"].value;
-  if (submit == "") {
-    alert ("Réservation interrompue, verifiez vos informations.");
+
+  // Si le formulaire est rempli correctement
+  if (isFirstNameCorrect && isLastNameCorrect && isEmailCorrect && isBirthdateCorrect && isQuantityCorrect && isCityCorrect && isCheckbox1Correct == true) {
+    // Confirmation du booking 
+    booking.style.display = "block";
+    return true;
+  }
+  // Sinon 
+  else {
+    alert("Réservation interrompue, verifiez vos informations.");
     return false;
   }
 }
-
-
